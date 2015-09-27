b046870ffe38151dcacf5398971da1cae05e410b
FlorianGoussin <florian.goussin@gmail.com>
2015-09-27 17:33:22 -0600

    add more dev dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,10 @@
 module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
@@ -12,14 +18,34 @@ module.exports = function(grunt) {
         }
       }
     },
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/bundle.min.js': ['dist/bundle.js'],
+        },
+      }
+    },
     watch: {
       css: {
-        files: 'app/style/*.scss',
-        tasks: ['sass']
+        files: ['app/style/*.scss', '<%= jshint.files %>'],
+        tasks: ['sass', 'jshint']
       }
     }
   });
-  grunt.registerTask('default', ['sass:dist', 'watch']);
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-}
+
+  grunt.registerTask('watch', ['watch']);
+  grunt.registerTask('dist', ['uglify']);
+
+};
