064f2e4d8af10eb8bc5234d43774ede17098cabe
FlorianGoussin <florian.goussin@gmail.com>
2015-10-12 01:01:21 -0600

    start to see a result...

--- app/app.js
+++ app/app.js
@@ -1,75 +1,72 @@
 // require dependencies
 var React = require('react');
+var ReactDOM = require('react-dom');
+
 var d3 = require('d3');
+
 var $ = require('jquery');
+var _ = require('lodash');
+
 var bootstrap = require('bootstrap');
 
-// var Chart = require('./Chart.jsx');
-
-// var sampleData = [
-//   {id: '5fbmzmtc', x: 7, y: 41, z: 6},
-//   {id: 's4f8phwm', x: 11, y: 45, z: 9},
-//   // ...
-// ];
 
 var DATA_PATH = 'app/data/budget-first-test.tsv';
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6"]);
 
 function loadBudgetData() {
   var deferred = $.Deferred();
 
-  d3.tsv(DATA_PATH, function(error, data) {
+  d3.tsv(DATA_PATH, function(error, cityData) {
     if (error) throw error;
 
+    // keep only the key total
+    color.domain(d3.keys(cityData[0]).filter(function(key) { 
+      return key == "TOTAL";
+    }));
+
+    // aggregate by department/ORGANIZATION
+    var departmentData = d3.nest()
+    .key(function(d) { return d.ORGANIZATION;})
+    .rollup(function(d) { 
+     return d3.sum(d, function(g) {return g.TOTAL; });
+   })
+    .entries(cityData);
+    console.log(JSON.stringify(departmentData, null, 2));
+
+    // calculate the city's full annual budget
+    var cityBudget = 0;
+    departmentData.forEach(function(d) {
+      cityBudget += parseFloat(d.values);
+    });
+
+    // calculate relative values
+    departmentData.forEach(function(d) {
+      d.budgets = color.domain().map(function(name) {
+        return {name: 'total', amount: +d.values};
+      });
+      d.budgets.push({name: 'remainder', amount: cityBudget - d.values});
+      d.percentage = d.values / cityBudget;
+      console.log(JSON.stringify(d, null, 2));
+    });
+
     // resolve the promise and pass the data
-    deferred.resolve(data);
+    deferred.resolve(departmentData, cityBudget);
   });  
 
   return deferred.promise();
 }
 
 var App;
-loadBudgetData().done(function(data) {
-  // App = React.createClass({
-  //   getInitialState: function() {
-  //     var colorRange = d3.scale.ordinal()
-  //         .range(["#98abc5", "#8a89a6"]);
-
-  //     // keep only the key total
-  //     colorRange.domain(d3.keys(cityData[0]).filter(function(key) { 
-  //       return key == "TOTAL";
-  //     }));
-
-  //     return {
-  //       data: data,
-  //       colorRange: colorRange,
-  //       radius: 74,
-  //     };
-  //   },
-
-  //   render: function() {
-  //     return (
-  //       <div className="App">
-  //         <Chart
-  //           data={this.state.data}
-  //           domain={this.state.colorRange}
-  //           domain={this.state.radius} />
-  //       </div>
-  //     );
-  //   }
-  // });
-
-  // React.renderComponent(App(), document.body);
-
-
-
-
+loadBudgetData().done(function(departmentData, cityBudget) {
 
   var radius = 74;
 
   var Chart = React.createClass({
     render: function() {
       return (
-        <svg class="pie" width={this.props.width} height={this.props.height}>{this.props.children}</svg>
+        <svg className="pie" width={this.props.width} height={this.props.height}>{this.props.children}</svg>
       );
     }
   });
@@ -77,8 +74,9 @@ loadBudgetData().done(function(data) {
   var Sector = React.createClass({
     render: function() {
       var arc = d3.svg.arc()
-        .outerRadius(240)
+        .outerRadius(radius)
         .innerRadius(0);
+
       return (
         <g className="arc">
           <path d={arc(this.props.data)}></path>
@@ -89,16 +87,19 @@ loadBudgetData().done(function(data) {
 
   var DataSeries = React.createClass({
     render: function() {
-
-      var pie = d3.layout.pie()
-      var bars = _.map(pie(this.props.data), function(point, i) {
+      var pie = d3.layout.pie();
+      var amounts = _.map(this.props.data.budgets, function(budget) {
+        return budget.amount;
+      });
+      var bars = _.map(pie(amounts), function(point, i) {
         return (
-          <Sector data=key{point} ={i}/>
+          <Sector data={point} key={i}/>
         )
       });
 
       return (
-        <g transform="translate(" + radius + ", " + radius + ")">{bars}</g>
+        <g transform="translate(37, 37)">{bars}</g>
+        // <g transform="translate(" + radius + ", " + radius + ")">{bars}</g>
       );
     }
   });
@@ -114,16 +115,18 @@ loadBudgetData().done(function(data) {
 
       return (
         <Chart width={this.props.width} height={this.props.height}>
-          <DataSeries data={data} width={this.props.width} height={this.props.height}  />
+          <DataSeries data={this.props.data} width={this.props.width} height={this.props.height}  />
         </Chart>
       );
     }
   });
   
-  var data = [2704659, 4499890, 2159981, 3853788, 14106543, 8819342, 612463]
+  // var data = [2704659, 4499890, 2159981, 3853788, 14106543, 8819342, 612463]
 
-  React.renderComponent(
-    <PieChart data={data}/>,
-    document.getElementById('container')
-  );
+  departmentData.forEach(function(d) {
+    ReactDOM.render(
+      <PieChart data={d}/>,
+      document.getElementById('container')
+    );
+  });
 });
