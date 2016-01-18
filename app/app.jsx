49259036cbd15694404dba15ba1ddb105ea5543e
FlorianGoussin <florian.goussin@gmail.com>
2016-01-18 08:20:08 -0700

    hamburger state

--- app/app.jsx
+++ app/app.jsx
@@ -103,12 +103,20 @@ dataInit(dataPath, color).done(function(cityData, cityBudget) {
       return 'menu-items ' + (this.state.active ? 'active' : '');
     }
 
+    isVisible(state) {
+      var state = typeof state != 'undefined' ? true : false;
+      return this.state.active == state ? { display: 'none' } : {};
+    }
+
     render() {
       var currentURL = window.location.href,
           barChartsURL = currentURL[currentURL.length-1] == '/' ? currentURL + 'bar-charts' : currentURL + '/bar-charts';
       return (
         <div className="Hamburger-menu-wrapper">
-          <div className='hamburger-menu' onClick={this.setFilter.bind(this, !this.state.active)}><i className="fa fa-bars"></i></div>
+          <div className='hamburger-menu' onClick={this.setFilter.bind(this, !this.state.active)}>
+            <i className="fa fa-bars" style={this.isVisible(true)}></i>
+            <i className="fa fa-times" style={this.isVisible()}></i>
+          </div>
           <ul className={this.isActive()}>
             <li><Link to={currentURL}>Pie charts</Link></li>
             <li><Link to={barChartsURL}>Bar charts</Link></li>
